3c07b4398f3f95feb61f3ab8cbf5b26ee52ab521
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-11-01 22:15:18 +0100

    Color display very good

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ document.body.style = "margin: 0; padding: 0; box-sizing: border-box;"
 const hDiv = document.createElement("div");
 hDiv.style = "display: flex; flex-direction: column;"
 
+let hue = 120;
 
 for (let index = 5; index > 0; index--) {
     const createH = document.createElement(`h${index}`)
     createH.textContent = `Rad ${6 - index}`
+    createH.style = `background-color: hsl(${hue} 89.6% 84.9%); color: #6E6EF7; text-align: center;`
     hDiv.appendChild(createH)
-
+    hue = hue + 20;
 }
 
+
 document.body.appendChild(hDiv)
+
+// start color: #B6FBB6
+
+//end color: #B6E4FB
+
 
 
 
